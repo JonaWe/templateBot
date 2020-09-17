167675b6f15282b4b753368f85e0fcd4c5f9727c
Jona Wessendorf <jonawessendorf@gmail.com>
2020-09-18 01:30:29 +0200

    changes display style of the command error handler

--- cogs/events/CommandErrorEvent.py
+++ cogs/events/CommandErrorEvent.py
@@ -21,73 +21,73 @@ class CommandErrorEvent(commands.Cog):
         if is_error_type(commands.CommandError):
             if is_error_type(commands.UserInputError):
                 if is_error_type(commands.TooManyArguments):
-                    embed.description = f"**Error:**```Too many arguments for this command!```"
+                    embed.description = f"**Error:**```fix\nToo many arguments for this command!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
                 elif is_error_type(commands.BadArgument):
                     if is_error_type(commands.MessageNotFound):
-                        embed.description = f"**Error:**```I could not find the message!```"
+                        embed.description = f"**Error:**```fix\nI could not find the message!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.MemberNotFound):
-                        embed.description = f"**Error:**```I could not find the member!```"
+                        embed.description = f"**Error:**```fix\nI could not find the member!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.UserNotFound):
-                        embed.description = f"**Error:**```I could not find the user!```"
+                        embed.description = f"**Error:**```fix\nI could not find the user!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.ChannelNotFound):
-                        embed.description = f"**Error:**```I could not find the channel!```"
+                        embed.description = f"**Error:**```fix\nI could not find the channel!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.ChannelNotReadable):
-                        embed.description = f"**Error:**```The channel is not readable for me!```"
+                        embed.description = f"**Error:**```fix\nThe channel is not readable for me!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.BadColourArgument):
-                        embed.description = f"**Error:**```The colour is invalid!```"
+                        embed.description = f"**Error:**```fix\nThe colour is invalid!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.RoleNotFound):
-                        embed.description = f"**Error:**```I could not find the role!```"
+                        embed.description = f"**Error:**```fix\nI could not find the role!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.EmojiNotFound):
-                        embed.description = f"**Error:**```I could not find the emoji!```"
+                        embed.description = f"**Error:**```fix\nI could not find the emoji!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(commands.BadBoolArgument):
-                        embed.description = f"**Error:**```That is not a valid boolean!```"
+                        embed.description = f"**Error:**```fix\nThat is not a valid boolean!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     elif is_error_type(customErrors.errors.SubCommandRequired):
-                        embed.description = f"**Error:**```A subcommand is required in order to run this command!```"
+                        embed.description = f"**Error:**```fix\nA subcommand is required in order to run this command!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                     else:
-                        embed.description = f"**Error:**```Invalid argument for this command!```"
+                        embed.description = f"**Error:**```fix\nInvalid argument for this command!```"
                         embed.add_field(name="\uFEFF",
                                         value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                         await ctx.send(embed=embed)
                 else:
-                    embed.description = f"**Error:**```You have used this command incorrectly!```"
+                    embed.description = f"**Error:**```fix\nYou have used this command incorrectly!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
             elif is_error_type(commands.CommandNotFound):
-                embed.description = f"**Error:**```This command does not exist!```"
+                embed.description = f"**Error:**```fix\nThis command does not exist!```"
                 embed.add_field(name="\uFEFF",
                                 value=f"Use `{ctx.prefix}help` for a list of all the commands.")
                 await ctx.send(embed=embed)
@@ -97,7 +97,7 @@ class CommandErrorEvent(commands.Cog):
                 cooldown_type = error.cooldown.type
 
                 if cooldown_type == discord.ext.commands.BucketType.guild:
-                    embed.description = f"**Error:**```This command is on cooldown for this server!```" \
+                    embed.description = f"**Error:**```fix\nThis command is on cooldown for this server!```" \
                                         f"\nYou must wait **{wait_msg}** to use `{ctx.prefix}{ctx.command}` again!"
                 elif cooldown_type == discord.ext.commands.BucketType.channel:
                     embed.description = f"**Error:**```This command is on cooldown for this channel!```" \
@@ -111,24 +111,24 @@ class CommandErrorEvent(commands.Cog):
                 await ctx.send(embed=embed)
             elif is_error_type(commands.CheckFailure):
                 if is_error_type(commands.NoPrivateMessage):
-                    embed.description = f"**Error:**```This command does not work in DM messages!```"
+                    embed.description = f"**Error:**```fix\nThis command does not work in DM messages!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
                 elif is_error_type(commands.NSFWChannelRequired):
-                    embed.description = f"**Error:**```This command must be executed in a NSFW channel!```"
+                    embed.description = f"**Error:**```fix\nThis command must be executed in a NSFW channel!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
                 elif is_error_type(commands.MissingPermissions):
                     # todo list permissions
-                    embed.description = f"**Error:**```You dont have permissions to use this command!```"
+                    embed.description = f"**Error:**```fix\nYou dont have permissions to use this command!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
                 elif is_error_type(commands.BotMissingPermissions):
                     # todo list permissions
-                    embed.description = f"**Error:**```I dont have the required permissions to execute this command!```"
+                    embed.description = f"**Error:**```fix\nI dont have the required permissions to execute this command!```"
                     embed.add_field(name="\uFEFF",
                                     value=f"Use `{ctx.prefix}help {ctx.command}` for more information about this command.")
                     await ctx.send(embed=embed)
