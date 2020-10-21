c322e123b54fee8fe28c52e8e38a513983a6ce64
Jona Wessendorf <jonawessendorf@gmail.com>
2020-10-21 14:21:34 +0200

    adds the YTDLSource class from the discord.py voice example

--- cogs/commands/musicCommands.py
+++ cogs/commands/musicCommands.py
@@ -1,6 +1,50 @@
+import asyncio
+
 import discord
 from discord.ext import commands
+import youtube_dl
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def form_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if "entries" in data:
+            data = data[0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.PCMVolumeTransformer(filename, **ffmpeg_options), data=data)
 
 class MusicCommands(commands.Cog):
     def __init__(self, bot):
@@ -51,7 +95,7 @@ class MusicCommands(commands.Cog):
 
     @commands.command(description="Changes the volume of the bot")
     @commands.guild_only()
-    async def volume(self, ctx: commands.context.Context, level: int):
+    async def volume(self, ctx: commands.context.Context, level: int=50):
         vc = ctx.voice_client
 
         if not vc:
